08d9a5d2f4448b805ef2a92c9167273c970922f3
Jenny Branham <branhamjenny@gmail.com>
2021-01-07 10:16:35 -0600

    Add test museum exists

---
+++ test/museum_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/pride'
+require 'minitest/autorun'
+require './lib/museum'
+
+class MuseumTest < Minitest::Test
+  def test_it_exists
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_instance_of Museum, dmns
+  end
+end
