e0817c0a42e57245a6edffb91eaf4e193ddbd95e
Jenny Branham <branhamjenny@gmail.com>
2021-01-07 10:25:48 -0600

    Add test for recommended exhibits

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -2,6 +2,7 @@ require 'minitest/pride'
 require 'minitest/autorun'
 require './lib/museum'
 require './lib/exhibit'
+require './lib/patron'
 
 class MuseumTest < Minitest::Test
   def test_it_exists
@@ -24,5 +25,18 @@ class MuseumTest < Minitest::Test
     dmns.add_exhibit(imax)
 
     assert_equal [imax], dmns.exhibits
+  end
+
+  def test_recommend_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    recommended_exhibits = [dead_sea_scrolls, gems_and_minerals]
+
+    assert_equal recommended_exhibits, dmns.recommend_exhibits(patron_1)
   end
 end
