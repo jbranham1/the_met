3f30e64fd9c96c366a7c9438029cf8923311f849
Jenny Branham <branhamjenny@gmail.com>
2021-01-07 10:11:22 -0600

    Add test for readable attributes for patron

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -8,4 +8,12 @@ class PatronTest < Minitest::Test
 
     assert_instance_of Patron, patron
   end
+
+  def test_it_has_readable_attributes
+    patron = Patron.new("Bob", 20)
+
+    assert_equal "Bob", patron.name
+    assert_equal 20, patron.spending_money
+    assert_equal [], patron.interests
+  end
 end
