e13ba17e35ddb49f3db88efe1e699f704b2224d1
Jenny Branham <branhamjenny@gmail.com>
2021-01-07 10:35:40 -0600

    Add mocks and stubs to museum test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -3,6 +3,7 @@ require 'minitest/autorun'
 require './lib/museum'
 require './lib/exhibit'
 require './lib/patron'
+require 'mocha/minitest'
 
 class MuseumTest < Minitest::Test
   def test_it_exists
@@ -20,7 +21,7 @@ class MuseumTest < Minitest::Test
 
   def test_add_exhibit
     dmns = Museum.new("Denver Museum of Nature and Science")
-    imax = Exhibit.new({name: "IMAX",cost: 15})
+    imax = mock
 
     dmns.add_exhibit(imax)
 
@@ -29,16 +30,18 @@ class MuseumTest < Minitest::Test
 
   def test_recommend_exhibits
     dmns = Museum.new("Denver Museum of Nature and Science")
-    imax = Exhibit.new({name: "IMAX",cost: 15})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    imax = mock
+    dead_sea_scrolls = mock
+    gems_and_minerals = mock
+    dead_sea_scrolls.stubs(:name).returns("Dead Sea Scrolls")
+    gems_and_minerals.stubs(:name).returns("Gems and Minerals")
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(gems_and_minerals)
-    patron_1 = Patron.new("Bob", 20)
-    patron_1.add_interest("Dead Sea Scrolls")
-    patron_1.add_interest("Gems and Minerals")
+    patron = mock
+    patron.stubs(:interests).returns(["Dead Sea Scrolls","Gems and Minerals"])
+
     recommended_exhibits = [dead_sea_scrolls, gems_and_minerals]
 
-    assert_equal recommended_exhibits, dmns.recommend_exhibits(patron_1)
+    assert_equal recommended_exhibits, dmns.recommend_exhibits(patron)
   end
 end
